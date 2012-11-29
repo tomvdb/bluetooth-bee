b392af75515171fd72ad141f215b0aa28f729138
Tom Van den Bon <tvdbon@gmail.com>
2012-11-29 17:51:33 +0200

    initial commit

---
+++ bluetooth_bee/mainWindow.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace bluetooth_bee
+{
+  partial class mainWindow
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.ConnectButton = new System.Windows.Forms.Button();
+      this.debugList = new System.Windows.Forms.ListBox();
+      this.ConfigureButton = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.txtInput = new System.Windows.Forms.TextBox();
+      this.sendButton = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // ConnectButton
+      // 
+      this.ConnectButton.Location = new System.Drawing.Point(12, 12);
+      this.ConnectButton.Name = "ConnectButton";
+      this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+      this.ConnectButton.TabIndex = 0;
+      this.ConnectButton.Text = "Connect";
+      this.ConnectButton.UseVisualStyleBackColor = true;
+      this.ConnectButton.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // debugList
+      // 
+      this.debugList.FormattingEnabled = true;
+      this.debugList.Location = new System.Drawing.Point(12, 114);
+      this.debugList.Name = "debugList";
+      this.debugList.Size = new System.Drawing.Size(783, 394);
+      this.debugList.TabIndex = 1;
+      // 
+      // ConfigureButton
+      // 
+      this.ConfigureButton.Location = new System.Drawing.Point(93, 12);
+      this.ConfigureButton.Name = "ConfigureButton";
+      this.ConfigureButton.Size = new System.Drawing.Size(75, 23);
+      this.ConfigureButton.TabIndex = 2;
+      this.ConfigureButton.Text = "Configure";
+      this.ConfigureButton.UseVisualStyleBackColor = true;
+      this.ConfigureButton.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(12, 62);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(34, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Input:";
+      // 
+      // txtInput
+      // 
+      this.txtInput.Location = new System.Drawing.Point(52, 59);
+      this.txtInput.Name = "txtInput";
+      this.txtInput.Size = new System.Drawing.Size(562, 20);
+      this.txtInput.TabIndex = 4;
+      // 
+      // sendButton
+      // 
+      this.sendButton.Location = new System.Drawing.Point(620, 57);
+      this.sendButton.Name = "sendButton";
+      this.sendButton.Size = new System.Drawing.Size(75, 23);
+      this.sendButton.TabIndex = 5;
+      this.sendButton.Text = "Send";
+      this.sendButton.UseVisualStyleBackColor = true;
+      this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+      // 
+      // mainWindow
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(807, 524);
+      this.Controls.Add(this.sendButton);
+      this.Controls.Add(this.txtInput);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.ConfigureButton);
+      this.Controls.Add(this.debugList);
+      this.Controls.Add(this.ConnectButton);
+      this.Name = "mainWindow";
+      this.Text = "Bluetooth Bee";
+      this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.mainWindow_FormClosing);
+      this.Load += new System.EventHandler(this.mainWindow_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button ConnectButton;
+    private System.Windows.Forms.ListBox debugList;
+    private System.Windows.Forms.Button ConfigureButton;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox txtInput;
+    private System.Windows.Forms.Button sendButton;
+  }
+}
+
---
+++ bluetooth_bee/mainWindow.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace bluetooth_bee
+{
+  public partial class mainWindow : Form
+  {
+    private delegate void debugDelegate(string msg);
+
+    private SerialPort bluetoothSerial;
+
+    public mainWindow()
+    {
+      InitializeComponent();
+    }
+
+    static string ReceiveBuffer = "";
+    static string SerialData = "";
+
+    private bool OK = false;
+    private bool ERROR = false;
+
+    public void log(string msg)
+    {
+      if (debugList.InvokeRequired)
+      {
+        debugList.BeginInvoke(new debugDelegate(log), new object[] {  msg });
+      }
+      else
+      {
+        string dt = DateTime.Now.ToString("HH.mm.ss.FFF");
+        int i = debugList.Items.Add(dt + " " + msg);
+
+        debugList.TopIndex = i;
+      }
+    }
+
+    private void mainWindow_Load(object sender, EventArgs e)
+    {
+      bluetoothSerial = new SerialPort("com31", 38400);
+      bluetoothSerial.DataReceived += new SerialDataReceivedEventHandler(bluetoothSerial_DataReceived);
+    }
+
+    void bluetoothSerial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+      ReceiveBuffer += bluetoothSerial.ReadExisting();
+      processRX();
+    }
+
+    private void processRX()
+    {
+      lock (ReceiveBuffer)
+      {
+
+        while (ReceiveBuffer.Length > 1)
+        {
+          if (ReceiveBuffer.Substring(0, 2) == "\r\n")
+          {
+            if (!ReceiveBuffer.Substring(2).Contains("\r\n")) // need more data
+              return;
+
+            // get command
+            string command = ReceiveBuffer.Substring(2, ReceiveBuffer.Substring(2).IndexOf("\r\n"));
+
+            log("Command: " + command);
+            ReceiveBuffer = ReceiveBuffer.Substring(command.Length + 4);
+
+            if (command == "OK") OK = true;
+            if (command == "ERROR") ERROR = true;
+          }
+          else
+          {
+            SerialData += ReceiveBuffer.Substring(0, 1);
+            ReceiveBuffer = ReceiveBuffer.Substring(1);
+          }
+        }
+
+        if (SerialData.Length > 0)
+        {
+          log("SER: " + SerialData);
+          SerialData = "";
+        }
+      }
+    }
+
+    private void sendCommand(string cmd)
+    {
+      OK = false;
+      ERROR = false;
+
+      if (!bluetoothSerial.IsOpen)
+      {
+        log("Open Connection First!");
+        return;
+      }
+
+      log(">>" + cmd);
+
+      bluetoothSerial.Write("\r\n" + cmd + "\r\n");
+    }
+
+    private bool sendCommand(string cmd, bool waitForResponse)
+    {
+      sendCommand(cmd);
+
+      while (!OK && !ERROR)
+        Application.DoEvents();
+
+      if (OK) return true;
+      else
+        return false;
+    }
+
+
+    private void configure()
+    {
+      if (!bluetoothSerial.IsOpen)
+      {
+        log("Open Connection First!");
+        return;
+      }
+
+      sendCommand("+RTADDR", true); // get module address
+      sendCommand("+STWMOD=0", true); // mode
+      sendCommand("+STNA=bluetoothbee", true); // setname
+      sendCommand("+STAUTO=0", true); // auto connect last paired device
+      sendCommand("+STOAUT=1", true); // permit paired device to connect
+      sendCommand("+DLPIN", true); // delete pin
+      sendCommand("+STPIN=1234", true); // set pin
+      Thread.Sleep(2000);
+      sendCommand("+INQ=1", true); // start
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if (bluetoothSerial.IsOpen)
+        bluetoothSerial.Close();
+
+      bluetoothSerial.Open();
+
+      log("Open");
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      configure();
+    }
+
+    private void mainWindow_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      if (bluetoothSerial.IsOpen)
+        bluetoothSerial.Close();
+    }
+
+    private void sendButton_Click(object sender, EventArgs e)
+    {
+      if (bluetoothSerial.IsOpen)
+        bluetoothSerial.WriteLine(txtInput.Text);
+    }
+  }
+}
